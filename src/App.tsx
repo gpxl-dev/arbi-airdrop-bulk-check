fe226d4744efa30fbb3ff6acbaa47f41138a6494
greypixel <greypixel@gpxl.tech>
2023-03-17 11:10:19 +0000

    Update address

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ import { ImSpinner10 } from "react-icons/im";
 const getCheckUrl = (address: string) =>
   "https://corsproxy.io/?" +
   encodeURIComponent(
-    `https://arbitrum.foundation/_next/data/NDhqybgYBJYIbHFAh1PQB/eligibility.json?address=${address.toLowerCase()}`
+    `https://arbitrum.foundation/_next/data/wX04UisPEjc-aIaKHSFBM/eligibility.json?address=${address.toLowerCase()}`
   );
 
 type CheckResult = {
